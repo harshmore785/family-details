e5b919145e0f751a002060bfdfe233668f392d8d
moreharsh018 <moreharsh018@gmail.com>
2024-11-15 12:38:42 +0530

    changes in migration

--- database/migrations/2024_11_15_060616_create_family_heads_table.php
+++ database/migrations/2024_11_15_060616_create_family_heads_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\City;
+use App\Models\State;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,8 +20,8 @@ return new class extends Migration
             $table->date('birthdate');
             $table->string('mobile_no');
             $table->string('address');
-            $table->string('state_id');
-            $table->string('city_id');
+            $table->foreignIdFor(State::class)->nullable()->constrained();
+            $table->foreignIdFor(City::class)->nullable()->constrained();
             $table->string('pincode');
             $table->string('marital_status');
             $table->date('wedding_date')->nullable();
--- database/migrations/2024_11_15_060802_create_family_members_table.php
+++ database/migrations/2024_11_15_060802_create_family_members_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\FamilyHead;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('family_members', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('family_head_id')->constrained('family_heads');
+            $table->foreignIdFor(FamilyHead::class)->constrained();
             $table->string('name');
             $table->date('birthdate');
             $table->string('marital_status');
